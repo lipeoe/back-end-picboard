9be0b5430990fc2c4214b3d567e7eb73479a9fde
Felipe <lipaosantos671@gmail.com>
2025-11-01 20:59:24 -0300

    remove: remoção de arquivo

--- models/DataGeneratos.js
+++
@@ -1,5 +0,0 @@
-class dataGenerator {
-    constructor(data_captura, hora, nome_estabelecimento, categoria_estabelecimento, bairro_estabelecimento, id_campanha, id_cupom, tipo_cupom, produto, valor_cupom, valor_compra, repasse_picmoney, local_captura, cep, zona){
-
-    }
-}
